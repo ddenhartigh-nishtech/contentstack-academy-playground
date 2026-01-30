1b8cd70035c728e2a27eb237c9e9ab0848bb4613
ddenhartigh <ddenhartigh@nishtechinc.com>
2026-01-30 17:41:50 -0500

    cut out all of the blog stuff from the /app and sitemap, left a bunch of irrelevant components for now.  Will build up my custom POC once I clear away the demo stuff.  Next task is to resolve the header error.

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -1,45 +1,46 @@
-'use client';
+"use client";
 
-import RenderComponents from '@/components/render-components';
-import { onEntryChange } from '@/contentstack-sdk';
-import { getPageRes, metaData } from '@/helper';
-import { Page as PageProp } from '@/typescript/pages';
-import { usePathname } from 'next/navigation';
-import React, { useState, useEffect } from 'react';
-import Skeleton from 'react-loading-skeleton';
-
+import RenderComponents from "@/components/render-components";
+import { onEntryChange } from "@/contentstack-sdk";
+import { getPageRes, metaData } from "@/helper";
+import { Page as PageProp } from "@/typescript/pages";
+import { usePathname } from "next/navigation";
+import React, { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
 
 export default function Page() {
-    const entryUrl = usePathname();
+	const entryUrl = usePathname();
 
-    const [getEntry, setEntry] = useState<PageProp>();
+	const [getEntry, setEntry] = useState<PageProp>();
 
-    async function fetchData() {
-        try {
-            const entryRes = await getPageRes(entryUrl);
-            if (!entryRes) throw new Error('Status code 404');
-            setEntry(entryRes);
-        } catch (error) {
-            console.error(error);
-        }
-    }
+	async function fetchData() {
+		try {
+			const entryRes = await getPageRes(entryUrl);
+			if (!entryRes) throw new Error("Status code 404");
+			console.log("[Page] Entry Data", entryRes);
+			setEntry(entryRes);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
-    useEffect(() => {
-        onEntryChange(() => fetchData());
-    }, []);
+	useEffect(() => {
+		onEntryChange(() => fetchData());
+	}, []);
 
-
-    return getEntry?.page_components ? (
-        <>
-            {getEntry.seo && getEntry.seo.enable_search_indexing && metaData(getEntry.seo)}
-            <RenderComponents
-                pageComponents={getEntry.page_components}
-                contentTypeUid='page'
-                entryUid={getEntry.uid}
-                locale={getEntry.locale}
-            />
-        </>
-    ) : (
-        <Skeleton count={3} height={300} />
-    );
+	return getEntry?.page_components ? (
+		<>
+			{getEntry.seo &&
+				getEntry.seo.enable_search_indexing &&
+				metaData(getEntry.seo)}
+			<RenderComponents
+				pageComponents={getEntry.page_components}
+				contentTypeUid="page"
+				entryUid={getEntry.uid}
+				locale={getEntry.locale}
+			/>
+		</>
+	) : (
+		<Skeleton count={3} height={300} />
+	);
 }
